1db37269e7c184e4a2a7d8bebc6a63e8e4efd360
XMG <XMG@163.com>
2016-03-21 15:24:49 +0800

    添加了person类

---
+++ weChat/weChat/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  weChat
+//
+//  Created by apple on 16/3/21.
+//  Copyright © 2016年 apple. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@end
---
+++ weChat/weChat/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  weChat
+//
+//  Created by apple on 16/3/21.
+//  Copyright © 2016年 apple. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
